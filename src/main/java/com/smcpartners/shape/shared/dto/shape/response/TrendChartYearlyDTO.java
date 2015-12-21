6dc90691977805dc4110c1a071d117e49347c219
Bryan Hokanson <bryanhokanson@Bryans-MacBook-Pro.local>
2015-12-21 11:32:32 -0500

    ShowAggregateComparison Service complete

--- src/main/java/com/smcpartners/shape/shared/dto/shape/response/TrendChartYearlyDTO.java
+++ src/main/java/com/smcpartners/shape/shared/dto/shape/response/TrendChartYearlyDTO.java
@@ -1,7 +1,14 @@
 package com.smcpartners.shape.shared.dto.shape.response;
 
+import java.util.List;
+
 /**
  * Created by bryanhokanson on 12/18/15.
  */
 public class TrendChartYearlyDTO {
+    private String name;
+    private String description;
+    private String numeratorDescription;
+    private String denominatorDescription;
+    private List<List<Object>> trendChart;
 }
